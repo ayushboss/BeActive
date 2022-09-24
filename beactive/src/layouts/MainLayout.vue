21a7e2dbb0f24d746b387a106450a3d671ddd072
JahnaviMahajan <mahajancodex@gmail.com>
2022-09-24 15:36:03 -0500

    Layout changes

--- beactive/src/layouts/MainLayout.vue
+++ beactive/src/layouts/MainLayout.vue
@@ -19,75 +19,70 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      >
+
       
-      <div class="User Profile" > 
-        <q-avatar size="8rem" style="margin-left:auto;margin-right:auto;margin-top:3rem;display:block;"><img src="../assets/images/blankPFP.jpg"></q-avatar>
-        <h5 style="text-align: center;">First Last</h5>
-      </div>
-
-      <q-separator />
-
-      <q-expansion-item
-        expand-separator
-        icon="today"
-        label="My Events"
-        caption="2 events"
-        default-opened
-      >
-        <q-expansion-item switch-toggle-side dense-toggle label="Climate Change Convention" :header-inset-level="0" :content-inset-level="1.3">
-          <q-card>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                eveniet doloribus ullam aliquid.
-          </q-card>
-        </q-expansion-item>
-
-        <q-expansion-item switch-toggle-side dense-toggle label="Black Lives Matter Movement" :header-inset-level="0" :content-inset-level="1.3">
-          <q-card>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                eveniet doloribus ullam aliquid.
-          </q-card>
-        </q-expansion-item>
-      </q-expansion-item>
-      
-      <q-expansion-item
-        expand-separator
-        icon="check"
-        label="My Interests"
-        caption="2 interested"
-        default-opened
-      >
-        <q-expansion-item switch-toggle-side dense-toggle label="Climate Change Convention" :header-inset-level="0" :content-inset-level="1.3">
-          <q-card>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                eveniet doloribus ullam aliquid.
-          </q-card>
-        </q-expansion-item>
-
-        <q-expansion-item switch-toggle-side dense-toggle label="Black Lives Matter Movement" :header-inset-level="0" :content-inset-level="1.3">
-          <q-card>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                eveniet doloribus ullam aliquid.
-          </q-card>
-        </q-expansion-item>
-      </q-expansion-item>
-
-      <q-btn>
+      <div class="grid_page">
+        <div class="profile_page">
+          <div class="user_profile" >
+            <q-avatar size="8rem" style="margin-left:auto;margin-right:auto;margin-top:3rem;display:block;"><img src="../assets/images/blankPFP.jpg"></q-avatar>
+            <h5 style="text-align: center;">First Last</h5>
+          </div>
+          <q-separator />
+          <div class="expansion_items" style= "margin: 10px solid red;">
+            <q-expansion-item
+              expand-separator
+              icon="today"
+              label="My Events"
+              caption="2 events"
+              default-opened
+            >
+              <q-expansion-item switch-toggle-side dense-toggle label="Climate Change Convention" :header-inset-level="0" :content-inset-level="1.3">
+                <q-card>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                      eveniet doloribus ullam aliquid.
+                </q-card>
+              </q-expansion-item>
+              <q-expansion-item switch-toggle-side dense-toggle label="Black Lives Matter Movement" :header-inset-level="0" :content-inset-level="1.3">
+                <q-card>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                      eveniet doloribus ullam aliquid.
+                </q-card>
+              </q-expansion-item>
+            </q-expansion-item>
+            <q-expansion-item
+              expand-separator
+              icon="check"
+              label="My Interests"
+              caption="2 interested"
+              default-opened
+            >
+              <q-expansion-item switch-toggle-side dense-toggle label="Climate Change Convention" :header-inset-level="0" :content-inset-level="1.3">
+                <q-card>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                      eveniet doloribus ullam aliquid.
+                </q-card>
+              </q-expansion-item>
+              <q-expansion-item switch-toggle-side dense-toggle label="Black Lives Matter Movement" :header-inset-level="0" :content-inset-level="1.3">
+                <q-card>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                      eveniet doloribus ullam aliquid.
+                </q-card>
+              </q-expansion-item>
+            </q-expansion-item>
+          </div>
+          <q-btn>
         
-      </q-btn>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+          </q-btn>
+        </div>
+        
+            <q-page-container class="main_page">
+        <router-view />
+            </q-page-container>
+      </div>
   </q-layout>
 </template>
 
