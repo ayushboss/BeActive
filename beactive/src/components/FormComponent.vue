6334d51ef08b6eb8b5356fb3eb5eecd3c8ccec4c
4Gleneyre <33353768+4Gleneyre@users.noreply.github.com>
2022-09-24 15:38:06 -0500

    made date and time correspond to same var

--- beactive/src/components/FormComponent.vue
+++ beactive/src/components/FormComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="q-pa-md" style="max-width: 400px">
-  
+      <h1>Create an event</h1>
       <q-form
         @submit="onSubmit"
         @reset="onReset"
@@ -13,23 +13,44 @@
           lazy-rules
           :rules="[ val => val && val.length > 0 || 'Please type something']"
         />
-  
+        
+        <div class="text-bold">Starting Date and Time</div>  
         <div class="dateTime">
           <q-date v-model="start" mask="YYYY-MM-DD HH:mm" color="purple" class="datePicker"/>
           <q-time v-model="start" mask="YYYY-MM-DD HH:mm" color="purple" />
         </div>
-        
+        <br />
+        <div class="text-bold">Ending Date and Time</div> 
         <div class="dateTime">
           <q-date v-model="end" mask="YYYY-MM-DD HH:mm" color="purple" class="datePicker"/>
           <q-time v-model="end" mask="YYYY-MM-DD HH:mm" color="purple" />
         </div>
-  
+        
+        <br />
+        <q-input
+            v-model="description"
+            filled
+            label="Event Description"
+            type="textarea"
+        />
+
+        <q-toggle v-model="includeContactInfo" label="I would like my contact info to be shown under this event" />
         <q-toggle v-model="accept" label="I accept the license and terms" />
   
         <div>
           <q-btn label="Submit" type="submit" color="primary"/>
           <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
         </div>
+
+        <q-select
+            filled
+            v-model="multiple"
+            multiple
+            :options="options"
+            label="Multiple"
+            style="width: 250px"
+        />
+        
       </q-form>
   
     </div>
@@ -46,16 +67,18 @@
       const title = ref(null)
       const accept = ref(false)
       const start = ref('2019-02-22 21:02')
-      const endDate = ref('2020/01/01')
-      const endTime = ref('10:00')
-  
+      const end = ref('2019-02-22 21:02')
+      const description = ref('')  
+      const includeContactInfo = ref(false)
+
       return {
         title,
         accept,
         start,
-        endDate,
-        endTime,
-  
+        end,
+        description,
+        includeContactInfo,
+
         onSubmit () {
           if (accept.value !== true) {
             $q.notify({
