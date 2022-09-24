eb903fd88cff049eaa9ee8d53c71994d476822bd
JahnaviMahajan <mahajancodex@gmail.com>
2022-09-24 18:05:28 -0500

    Finished fullpost

--- beactive/src/components/ExampleComponent.vue
+++ beactive/src/components/ExampleComponent.vue
@@ -1,47 +1,48 @@
-<template>
-  <div>
-    <div class="q-pa-md card__view">
-    <q-card class="my-card">
-      <q-img
-        src="https://cdn.quasar.dev/img/parallax2.jpg"
-        class="card__image"
-      />
-      
-
-      <q-card-section class="card__section-one">
-        <div class="card__header">
-          <h1 class="card__title">{{ title }}</h1>
-          <p class="card__ago"> posted 4 hours ago</p>
+<template class="temp">
+  <div class="full_view">
+        <q-banner inline-actions class="text-white bg-blue banner">
+        <p class="full_title"> Climate Change Convention</p>
+        <p class="full_time">posted 4 hours ago</p>
+        </q-banner>
+        <div class="full_view_list">
+          <q-list>
+          <q-item clickable>
+            <q-item-section avatar>
+              <q-icon color= "blue" name="watch"/>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Date/Time</q-item-label>
+              <q-item-label caption>24 Sept, 2024 | 5:00 pm - 6:00 pm</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item clickable>
+            <q-item-section avatar>
+              <q-icon color="red" name="place" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Location</q-item-label>
+              <q-item-label caption>RMC Grand Hall</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item clickable>
+            <q-item-section avatar>
+              <q-icon color= "green" name="phone"/>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Contact Info</q-item-label>
+              <q-item-label caption>jm139@rice.edu</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+        <div class="full_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dicta nobis mollitia eveniet voluptatum. Ad obcaecati voluptas accusantium, non, ipsa tempore veniam pariatur enim itaque temporibus esse aut, quo distinctio! Iure optio nisi incidunt, mollitia ipsa minima autem iste nesciunt ratione maxime ipsum labore rem adipisci assumenda, ea hic aspernatur!</div>
         </div>
-        <q-list>
-        <q-item clickable>
-          <q-item-section avatar>
-            <q-icon color= "blue" name="watch"/>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Date/Time</q-item-label>
-            <q-item-label caption>24 Sept, 2024 | 5:00 pm - 6:00 pm</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable>
-          <q-item-section avatar>
-            <q-icon color="red" name="place" />
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Location</q-item-label>
-            <q-item-label caption>RMC Grand Hall</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-      </q-card-section>
-      <q-separator color="blue" inset />
-      <q-card-section class=" card__section-two text-subitle2">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, sint sit ex, aliquid rem necessitatibus dolore reiciendis minima, modi facilis facere asperiores expedita in voluptatem!
-      </q-card-section>
-      <q-card-section class="card__section-three">
+        <q-separator />
+        <q-card-section class="card__section-three">
+          <div class="card__interested full">
+            <p>4.1K interested</p>
+        <q-btn class = "thin" color="red" label="Interested?" unelevated rounded/>
+      
+  </div>
       <div class="card__badge-list">
       <q-badge class= "card__badge-each" color="blue">
       Environment
@@ -53,18 +54,91 @@
       Future
       </q-badge>
       </div>
-      <q-btn color="red" label="Interested?" unelevated rounded/>
     </q-card-section>
-    <div class="card__interested">
-      <p>4.1K interested</p>
+    <q-separator />
+    <div>
+
     </div>
-    </q-card>
+    <div class="full_discussion">
+      <h1>Discussion</h1>
+      <div class="together">
+        
+        <p>Add comment</p>
+        <q-icon class = "comment-icon" color= "green" name="comment"/>
+        
+      </div>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label lines="1">Brunch this weekend?</q-item-label>
+          <q-item-label caption lines="2">
+            <span class="text-weight-bold">Janet</span>
+            -- I'll be in your neighborhood doing errands this
+            weekend. Do you want to grab brunch?
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+
+      <q-separator inset="item" />
+
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label lines="1">Linear Project</q-item-label>
+          <q-item-label caption lines="2">
+            <span class="text-weight-bold">John</span>
+            -- Can we schedule a call for tomorrow?
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+      <q-separator inset="item" />
+
+<q-item clickable v-ripple>
+  <q-item-section avatar>
+    <q-avatar>
+      <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+    </q-avatar>
+  </q-item-section>
+
+  <q-item-section>
+    <q-item-label lines="1">Linear Project</q-item-label>
+    <q-item-label caption lines="2">
+      <span class="text-weight-bold">John</span>
+      -- Can we schedule a call for tomorrow?
+    </q-item-label>
+  </q-item-section>
+
+  <q-item-section side top>
+    1 min ago
+  </q-item-section>
+</q-item>
+
+<q-separator inset="item" />
 
 
-    
-    
+
+      
+
     </div>
-  </div>
+</div>
 </template>
 
 
---
+++ beactive/src/components/fullpostComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>wassup</p>
+    </div>
+</template>
